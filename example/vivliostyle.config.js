fd0359e7d62afb284bb989e99a16da3bb9220562
Koutaro Mukai <mukai.k1011k@outlook.jp>
2025-09-24 23:34:22 +0900

    Update example

--- example/vivliostyle.config.js
+++ example/vivliostyle.config.js
@@ -1,13 +1,13 @@
 // @ts-check
 
-import { VFM } from "@vivliostyle/vfm";
 import fs from "node:fs";
 import path from "node:path";
-import { visit } from "unist-util-visit";
 
 import { hooks } from "playwright-core";
 
 hooks.pdf.before = async ({ page }) => {
+  process.stdout.write("\n");
+
   const html = await page.evaluate(
     () =>
       // eslint-disable-next-line no-undef
@@ -17,47 +17,21 @@ hooks.pdf.before = async ({ page }) => {
     encoding: "utf-8",
   });
 
-  const resolvedValues = await page.evaluate(() =>
-    // 文字列化されてブラウザで実行されるので、
-    // Elementを直接受け渡すことはできない
-
-    // eslint-disable-next-line no-undef
-    Array.from(document.querySelectorAll("[data-pseudo-id]"))
-      .filter((elem) => elem.querySelector("[data-vivliostyle-target-counter]"))
-      .map((elem) => ({
-        id: /** @type {string} */ (elem.getAttribute("data-pseudo-id")),
-        textContent: elem.textContent,
-      })),
+  const textContent = await page.evaluate(
+    () =>
+      // eslint-disable-next-line no-undef
+      document.querySelector(
+        '[data-pseudo-id="665ca512-9bd5-49c8-8431-a96fb88cd62b"]',
+      )?.textContent,
   );
-  fs.writeFileSync(
-    path.join(process.cwd(), "counters.json"),
-    JSON.stringify(resolvedValues),
-    { encoding: "utf-8" },
-  );
-  console.dir(resolvedValues);
+  process.stdout.write(`resolved: ${textContent}\n`);
 };
 
 /** @type {import('@vivliostyle/cli').VivliostyleConfigSchema} */
 const vivliostyleConfig = {
   title: "example",
-  author: "u1f992",
   theme: "./theme",
   entry: ["manuscript.md"],
-  documentProcessor: (opts, meta) =>
-    VFM(opts, meta).use(() => (root) => {
-      // Vivliostyleに処理されたあとで検索できるID値が必要
-      // id属性やhref属性は、文書全体で通るように処理の過程で置き換えられることがある
-      // data-*属性なら、Vivliostyleの内部処理で使う命名と衝突していなければ保持されるようだ
-      //
-      // .vivliostyle/manuscript.htmlとhooks.pdf.beforeが出力するcounters.jsonを突き合わせることで、
-      // target-counter()の解決済みの値がわかる
-
-      visit(/** @type {import("hast").Root} */ (root), "element", (elem) => {
-        if (elem.tagName === "a") {
-          (elem.properties ??= {})["data-pseudo-id"] = crypto.randomUUID();
-        }
-      });
-    }),
 };
 
 export default vivliostyleConfig;
